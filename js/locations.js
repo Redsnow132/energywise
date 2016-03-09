d634a17236b8b76e2dff8e96ad4071074a017ed0
FlamingGenius <livewiretech.reply.mail@gmail.com>
2016-03-09 14:46:31 -0500

    Create locations.js

---
+++ js/locations.js
@@ -0,0 +1,19 @@
+function showPanel1(){
+  if(displayPanel1 == "Oakhill"){
+   panel1.style.display = 'block';
+   displayPanel1.value = 'Hide';
+  } else if(displayPanel1 == 'Hide'){
+   displayPanel1.value = 'Oakhill';
+   panel1.style.display = 'none';
+  }
+ }
+ 
+ function showPanel2(){
+   if(displayPanel2 == "Oakhill"){
+   panel2.style.display = 'block';
+   displayPanel2.value = 'Hide';
+  } else if(displayPanel2 == 'Hide'){
+   displayPanel2.value = 'Oakhill';
+   panel2.style.display = 'none';
+  }
+ }
