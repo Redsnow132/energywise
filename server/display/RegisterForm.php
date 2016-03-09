be7200956fdccb8a72584f1858f5b07a6b577c42
FlamingGenius <livewiretech.reply.mail@gmail.com>
2016-03-09 14:39:24 -0500

    Create RegisterForm.php

---
+++ server/display/RegisterForm.php
@@ -0,0 +1,16 @@
+ <form method='post'>
+ <input type='text' name='first' placeholder='First Name'>
+ <input type='text' name='last' placeholder='Last Name'>
+ <br>
+ <input type='text' name='email' placeholder='Email'>
+ <br>
+ <input type='text' name='username' placeholder='Username'>
+ <br>
+ <input type='password' name='password' placeholder='Password'>
+ <br>
+ <input type='password' name='cpass' placeholder='Confirm Password'>
+ <br>
+ <input type='text' name='teacherCode' placeholder='Teacher Code'><b>Teachers Only</b>
+ <br>
+ <input type='submit' name='Register' value='Register'>
+ </form>
